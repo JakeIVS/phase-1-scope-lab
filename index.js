6e4f4717929cf085cfce6fa1106731610388ea7b
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-02 09:44:22 -0600

    lab in progress

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName="bob";
+
+function upperCaseCustomerName(){
+   let nameInCaps = customerName.toUpperCase;
+   return nameInCaps
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    const bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Karen"
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = "Kevin"
+    return leastFavoriteCustomer;
+}
